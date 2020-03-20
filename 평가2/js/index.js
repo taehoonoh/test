f3cd5ec2222d28c4c26da55c4cd897dff192d9bd
taehoonoh <dkrakcjstk14@gmail.com>
2020-03-20 18:21:44 +0900

    평가2

---
+++ 평가2/js/index.js
@@ -0,0 +1,55 @@
+var pop = document.querySelector('.pop');
+var popImg = pop.querySelector('.pop img');
+var popFig = pop.querySelector('figcaption')
+var container = document.querySelector('.container');
+var bt = pop.querySelectorAll('a')
+
+
+
+var imgList = container.querySelectorAll('.gallery img');
+var imgFig = container.querySelectorAll('figcaption');
+
+
+
+popImg.addEventListener('click',function(){
+
+   pop.classList.remove('ly-pop')
+});
+
+
+
+// popup
+for(let i =0; i<imgList.length; i++){
+imgList[i].addEventListener('click',function(){
+    pop.classList.add('ly-pop');
+    popImg.src = imgList[i].src;
+    popFig.textContent = imgFig[i].textContent
+
+});
+}
+
+// prev
+bt[0].addEventListener('click',function(){
+for(var k=0; k<imgList.length;k++){
+
+
+popImg.src = imgList[k].src
+
+popFig.textContent = imgFig[k].textContent
+}
+});
+
+// next
+bt[1].addEventListener('click',function(){
+for(var k=0; k<imgList.length;k--){
+
+        popImg.src = imgList[k].src;
+        popFig.textContent = imgFig[k].textContent
+        }
+
+});
+
+//var omg = 'O M G' 
+
+// if(오태훈<60){재평가를 보겠습니다..}
+//else{}
