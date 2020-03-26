41b61bdb704559ce525129a8527168eefe996c89
taehoonoh <dkrakcjstk14@gmail.com>
2020-03-26 18:10:12 +0900

    0326

---
+++ load/common.js
@@ -0,0 +1,22 @@
+$(function(){
+    //start
+    $('header').load("header.html nav");
+    
+    $('footer').load("header.html .foot");
+
+    $('section').load('main.html');
+
+    $('body').addClass('active');
+
+    
+
+// 셋타임아웃
+// 클릭할때
+// e.preeventDefault();
+// 10
+// fade out
+// var ddd d = 속성.attr('href')
+
+
+    //end
+});
